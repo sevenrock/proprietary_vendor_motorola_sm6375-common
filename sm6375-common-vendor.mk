301219ee2143d404b8f197dfcc9067e63023dc8b
Rohan Hasabe <rohanhasabe8@gmail.com>
2023-12-09 00:15:02 +0530

    rhodep: Drop soundfx/libspeakerbundle blob
    *Audioservice hates this soundfx blob, causing the deivce to bootloop
    
    * Abort message: 'incStrongRequireStrong() called on 0xf1829ef0 which isn't already owned'
    12-08 12:16:51.175  2635  2635 F DEBUG   :     r0  00000000  r1  00000a44  r2  00000006  r3  f17fbc70
    12-08 12:16:51.175  2635  2635 F DEBUG   :     r4  f17fbc80  r5  f17fbc68  r6  00000a2d  r7  0000016b
    12-08 12:16:51.175  2635  2635 F DEBUG   :     r8  00000000  r9  ffffffff  r10 f17fbc70  r11 f5900f7c
    12-08 12:16:51.175  2635  2635 F DEBUG   :     ip  00000a44  sp  f17fbc50  lr  f633e603  pc  f633e616
    12-08 12:16:51.175  2635  2635 F DEBUG   : 9 total frames
    12-08 12:16:51.175  2635  2635 F DEBUG   : backtrace:
     int)+70) (BuildId: 49d0c39e812bf2c37927eb71be0e2773)
    12-08 12:16:51.175  2635  2635 F DEBUG   :       #05 pc 00009b21  /vendor/lib/soundfx/libspeakerbundle.so (MshSensorListener::enableSensor_L()+92) (BuildId: 6fcee247db5c64b8484cc43ba99161df)
    12-08 12:16:51.175  2635  2635 F DEBUG   :       #06 pc 00009961  /vendor/lib/soundfx/libspeakerbundle.so (MshSensorListener::enableThreadEntrance(void*)+20) (BuildId: 6fcee247db5c64b8484cc43ba99161df)
    
    Signed-off-by: Rohan Hasabe <rohanhasabe8@gmail.com>
    
    Original patch:
    https://github.com/aospa-rhodep/vendor_motorola_rhodep/commit/fa0ff09b112d7c94d7c49be7c029a0d2f8f08e4a

--- sm6375-common-vendor.mk
+++ sm6375-common-vendor.mk
@@ -663,7 +663,6 @@ PRODUCT_COPY_FILES += \
     vendor/motorola/sm6375-common/proprietary/vendor/lib/soundfx/libqcreverb.so:$(TARGET_COPY_OUT_VENDOR)/lib/soundfx/libqcreverb.so \
     vendor/motorola/sm6375-common/proprietary/vendor/lib/soundfx/libqcvirt.so:$(TARGET_COPY_OUT_VENDOR)/lib/soundfx/libqcvirt.so \
     vendor/motorola/sm6375-common/proprietary/vendor/lib/soundfx/libshoebox.so:$(TARGET_COPY_OUT_VENDOR)/lib/soundfx/libshoebox.so \
-    vendor/motorola/sm6375-common/proprietary/vendor/lib/soundfx/libspeakerbundle.so:$(TARGET_COPY_OUT_VENDOR)/lib/soundfx/libspeakerbundle.so \
     vendor/motorola/sm6375-common/proprietary/vendor/lib/vendor.qti.diaghal@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib/vendor.qti.diaghal@1.0.so \
     vendor/motorola/sm6375-common/proprietary/vendor/lib/vendor.qti.hardware.bluetooth_sar@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib/vendor.qti.hardware.bluetooth_sar@1.0.so \
     vendor/motorola/sm6375-common/proprietary/vendor/lib/vendor.qti.hardware.bluetooth_sar@1.1.so:$(TARGET_COPY_OUT_VENDOR)/lib/vendor.qti.hardware.bluetooth_sar@1.1.so \
